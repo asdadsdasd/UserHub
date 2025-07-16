6662fd8ccf45228120165cc8b7d5136342c01c1e
asdadsdasd <mkozarez12345@mail.ru>
2025-07-16 09:17:33 +0300

    init commit

---
+++ src/test/java/com/example/userhub/UserHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
