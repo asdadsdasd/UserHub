a680db5ed841aec39f6e4623fbd4c214c0231f58
asdadsdasd <mkozarez12345@mail.ru>
2025-07-23 16:42:01 +0300

    Added image caching to nginx

--- src/main/java/com/example/userhub/config/WebConfig.java
+++ src/main/java/com/example/userhub/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.example.userhub.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/images/**")
-                .addResourceLocations("file:/app/images/avatars/");
+                .addResourceLocations("file:/app/images/avatars/")
+                .setCacheControl(CacheControl.maxAge(60, TimeUnit.MINUTES).cachePublic());
     }
-
 }
